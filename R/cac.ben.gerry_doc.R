41b40b431b1b554068df2a3dc5ecbea70f861ad5
Kilem L. Gwet <gwet@agreestat.com>
2019-09-23 14:10:02 +0000

    version 1.0

---
+++ R/cac.ben.gerry_doc.R
@@ -0,0 +1,15 @@
+#' Ratings of 12 units from 2 raters named Ben and Gerry
+#' 
+#' This dataset contains ratings that 2 raters named Ben and Gerry assigned to 12 units distributed in 2 groups 
+#' "G1" and "G2".
+#' @format Each row of this dataset describes an interval and the interpretation of the magnitude it represents.
+#'  \describe{
+#'     \item{Group}{Group Name}
+#'     \item{Units}{Unit number}
+#'     \item{Ben}{Ben's Ratings}
+#'     \item{Gerry}{Gerry's Ratings}
+#'  }   
+#' The first 2 columns "Group" and "Units" play a descriptive role here and are not used by any fucntion included in this
+#' package. One will typically use cac.ben.gerry[,c(3,4)] or cac.ben.gerry[,c("Ben","Gerry")] as input 
+#' dataset.
+"cac.ben.gerry"
